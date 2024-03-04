dd46d33834ee76f95aa3cf4b7c9a3b8e804361ad
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:37:44 -0600

    tema 3

---
+++ _003_como_concatenar.py
@@ -0,0 +1,16 @@
+
+#practica 1
+variable_1 = "Mario" #creas una variable
+variable_2 = "Gomez" # tu segunda variable
+
+variable_3 = variable_1 + " " + variable_2 + "." # en esta tercer variable se guardaran las dos anteriores 
+
+print(variable_3) # imprime
+
+#practica 2
+num1= '19'  #creamos 3 variables
+num2= '02'
+num3= '2001'
+
+suma= num1+"/"+num2+"/"+num3+"." # es guardada en una tercera
+print(suma) # imprime
