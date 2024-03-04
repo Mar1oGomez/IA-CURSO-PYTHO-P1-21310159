8356b842a08762151f416c4fa6b3af180bbf23ce
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:46:06 -0600

    tema 10

---
+++ _010_posiciones_negativas_de_listas_Python.py
@@ -0,0 +1,5 @@
+#listas 2 posisciones negativas
+color= ['naranja','amrillo','lila','blanco','rojo']#string 
+print(color[-2]) #imprecion 'los numeros negativos' son del la lista del ultimo en adelante
+
+
