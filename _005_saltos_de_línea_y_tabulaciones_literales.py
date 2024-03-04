fe07a5c388d390a4e7568ed9cfb2313743145a09
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:41:07 -0600

    tema 5

---
+++ _005_saltos_de_línea_y_tabulaciones_literales.py
@@ -0,0 +1,3 @@
+
+print('Aprendiendo a saltar lineas y tabular.\n\t -Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.\n\t-Rust.\n\t-C++.')
+#\n hace los saltos de linea, mientras que \t tabula el texto, optimizando lineas en una sola
