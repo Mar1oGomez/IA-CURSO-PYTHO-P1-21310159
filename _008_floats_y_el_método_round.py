b48905fea3df96deca9d277064bb84ee8b85beed
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:43:54 -0600

    tema 8

---
+++ _008_floats_y_el_método_round.py
@@ -0,0 +1,14 @@
+
+#metodos floats y funcion round
+#float es "."
+#round redondea los decimales
+
+numero1= 24.456 #string
+numero2= 16.432 #string
+resultado = numero1+numero2 
+
+print(resultado) #impresion
+
+#Practica
+numero3= 17.567383292929200234 #string
+print(round(numero3,3))# metodo round
