b8d53b2ef285e7e6b5b3ffef28f26d4d65ba0b8a
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:48:48 -0600

    tema 13

---
+++ _013_eliminar_elementos_metodo_pop.py
@@ -0,0 +1,8 @@
+color = ['rojo', 'azul', 'verde', 'amarillo', 'cafe', 'morado', 'negro', 'rosa', 'blanco', 'naranja'] #string
+
+GuardeList1= color.pop(1) #pop guarda y elimina un elemento de la lista
+GuardeList2= color.pop(7) #pop guarda y elimina un elemento de la lista
+
+print(color) #imprime la lista color
+print('los colores eliminados de la lista y guardado:'+GuardeList1) #imprime lo guardado que se elimino
+print('los colores eliminados de la lista y guardado:'+GuardeList2) #imprime lo guardado que se elimino
