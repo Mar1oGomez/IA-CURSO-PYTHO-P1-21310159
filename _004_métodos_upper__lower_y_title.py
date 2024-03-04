c87bd791e6bc323f17611c37487ca5eb4061d722
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:38:25 -0600

    Add files via upload

---
+++ _004_métodos_upper__lower_y_title.py
@@ -0,0 +1,13 @@
+
+# Formatos de texto
+#Title
+Variable_1= 'mario ALBERTO gOmEz teMores' #variable string
+print(Variable_1.title())#primera letra del texto en mayuscula
+
+#Lower
+variable_2= 'PABLITO CLAVO UN CLAVITO'#variable string
+print(variable_2.lower())#todo en minuscula
+
+#Upper
+variable_3= 'pablito clavo un clavito'#variable string
+print(variable_3.upper()) #todo en mayuscula
