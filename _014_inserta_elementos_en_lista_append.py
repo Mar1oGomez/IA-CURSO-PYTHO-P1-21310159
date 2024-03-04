ff8b38c082fe816d08d09d4e53fbf15c3d0509f6
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:49:08 -0600

    tema 14

---
+++ _014_inserta_elementos_en_lista_append.py
@@ -0,0 +1,9 @@
+#listas 6
+#anade los colores'fuxia'y 'celeste'y maracuya XD
+color = ['rojo', 'azul', 'verde', 'amarillo', 'Cafe', 'Morado', 'negro', 'rosa', 'blanco', 'naranja']#string
+
+color.append('fuxia') #agregaremos a la lista
+color.append('celeste') #agregaremos a la lista
+color.append('maracuya XD') #agregaremos a la lista
+
+print(color) # imprecion
