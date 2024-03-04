d4f5b080bed97ef48c301c531d47af10c104bdc1
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:46:39 -0600

    tema 11

---
+++ _011__Eliminación_de_elementos_con_del.py
@@ -0,0 +1,8 @@
+#listas 3
+#practica
+color=['rojo','azul','verde','amarillo','cafe','morado','negro','rosa','blanco','naranja']# estrin con valores 
+del color[1] #borrara un valor de la lista
+del color[5] #borrara un valor de la lista
+del color[-3] #borrara un valor de la lista
+del color[-4] #borrara un valor de la lista
+print(color)#imprime
