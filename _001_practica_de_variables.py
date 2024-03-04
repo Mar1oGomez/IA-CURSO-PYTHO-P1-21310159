09da4a35f2772ff365ff70b5fcb61cca8ba744f0
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:29:41 -0600

    tema 1

---
+++ _001_practica_de_variables.py
@@ -0,0 +1,9 @@
+mensaje = "mi primer progama" #creas tu variable de texto
+
+mensaje2 = 10 # creas una variable numerica
+
+print(mensaje) #imprmes el mensaje
+
+print(mensaje2 + 35) # imprimes la variable numerica
+
+
