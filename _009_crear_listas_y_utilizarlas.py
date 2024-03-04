bfa3855f372f1e4b835daf72f8436d5e528140da
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:45:32 -0600

    tema 9

---
+++ _009_crear_listas_y_utilizarlas.py
@@ -0,0 +1,15 @@
+#listas 1
+celulares=('Xiaomi' , 'Iphone' , 'Huawei' , 'Samsung') #string
+print(celulares[3]) # imprecion y '[]' es donde se marca la lista
+
+#ejercicio1 'posicion 3
+color= ['rojo','azul','verde','amarillo','marron','lila','negro','rosa'] #string de la lista
+print(color[3]) #imprecion
+
+#ejercicio2 'posicion rojo y rosa seria el 0 y 7
+print(color[0]) #inprescion
+print(color[7])
+
+#ejercicio3 crea una lista que contenga los siguientes valores en las posiciones indicadas.
+numeros=('tres','dos','cinco','cuatro','uno') #string de la lista
+print(numeros) #imprecion
