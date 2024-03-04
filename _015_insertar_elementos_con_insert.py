9427b99ca54f03f099d7ca513613cd5ebb9f3364
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:50:07 -0600

    tema 15

---
+++ _015_insertar_elementos_con_insert.py
@@ -0,0 +1,6 @@
+#listas 7
+color = ['rojo', 'azul', 'verde', 'amarillo', 'cafe', 'morado', 'negro', 'rosa', 'blanco', 'naranja'] #string
+color.insert(2,'pitalla XD') #insertamos a la lista en un lugar en espicifico
+color.insert(0,'melon XD')#insertamos a la lista en un lugar en espicific
+print(color) #imprecion
+
