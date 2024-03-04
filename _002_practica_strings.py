99b586a0f3f24c6a732d3cd760f2b2e0ae738544
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:31:49 -0600

    tema 2

---
+++ _002_practica_strings.py
@@ -0,0 +1,8 @@
+"aprendiendo string" #una string se crea con el uso de comillas
+'aprendiendo string'
+
+texto= "aprendiendo sobre 'string'" #y puedes usar comillas dentro de estas mientras sean las contrarias
+texto2= 'aprendiendo sobre "string"' #y puedes usar comillas dentro de estas mientras sean las contrarias
+
+print(texto)
+print(texto2)
