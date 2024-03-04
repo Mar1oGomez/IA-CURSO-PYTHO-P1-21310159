ec8546163d9e43d31fa6d4eb2449346ace453b58
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:42:00 -0600

    tema 6

---
+++ _006__suma_resta_multiplicacion_y_division.py
@@ -0,0 +1,8 @@
+#practica con numeros enteros 
+# suma 
+suma= 24+33 #variable
+resta= 35-12 #variable
+multiplicacion= 12*78 #variable
+division= 356/14 #variable
+print(suma,resta,multiplicacion,division) #imprecion y la ","sirve para conatenar casos numericos
+
