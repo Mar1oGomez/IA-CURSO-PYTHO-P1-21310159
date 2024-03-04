0d1fc71d41c8a8b8200e0a27489c90a2bb8e0eb4
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-03 23:47:49 -0600

    tema 12

---
+++ _012__Eliminar_elementos_en_listas_Python_con_remove.py
@@ -0,0 +1,5 @@
+#eliminar listas
+color=['rojo','azul','verde','amarillo','cafe','morado','negro','rosa','blanco','naranja']# string
+color.remove('amarillo') #elimina color
+color.remove('rojo') #elimina color
+print(color) #imprecion
