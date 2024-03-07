3094458e7506ced4f276c8c4dc2048dcb44cf945
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 12:39:15 -0600

    17

---
+++ _017_Contar_la_longitud_de_elementos_de_una_lista.py
@@ -0,0 +1,3 @@
+#Contar la longitud de elementos de una lista
+XD=['mario','tito','paco','taco','pera','taza','pollo','loro','perry','agente p','dios','maradona','messy','el bicho','siuuuuuuu'] #string
+print(len(XD)) #len funciona para contar elementos de la lista 
