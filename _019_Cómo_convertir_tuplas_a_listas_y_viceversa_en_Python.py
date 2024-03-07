e8d52e73e6695238247657a3ae75423d4ea3000f
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 13:39:19 -0600

    tema 19

---
+++ _019_Cómo_convertir_tuplas_a_listas_y_viceversa_en_Python.py
@@ -0,0 +1,9 @@
+#Como convertir tuplas a listas y viceversa en Python
+
+lista=['negro','blanco','rosa','nigga'] #string
+tupla= tuple(lista) #variable y convertor de lista a tupla 
+print(tupla) #imprecion 
+
+tupla2=('mole','pollo','col','pinole') #string
+lista2= list(tupla2) #variable y convertor de tupla a lista
+print(lista2) #imprecion 
