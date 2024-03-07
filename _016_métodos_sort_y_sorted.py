9efd0223470315e0daebcb79c819dd0ead3af6d5
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 12:30:52 -0600

    tema 16

---
+++ _016_métodos_sort_y_sorted.py
@@ -0,0 +1,15 @@
+#listas 8
+color=['rojo','negro','cafe','blanco','verde','morado' ] #string
+color.sort() #sort funciona para ordenar alfabeticamente
+print(color) #imprime 
+
+nombre=['paco','rodo','hector','mario','alan','nestor'] #string
+nombre.sort(reverse=True) #ordena al reves
+print(nombre) #imprime
+
+food=['mole','tinga','pizza','rajas','pollo feliz'] #string
+print(sorted(food)) #ordena de forma alfabetica pero no es definitivo
+print(food) #imprime segun lo puesto
+
+
+
