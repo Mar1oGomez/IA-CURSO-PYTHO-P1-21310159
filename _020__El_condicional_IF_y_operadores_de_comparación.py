a8da1ab7464a7dd5d5c8b5c964c6cbe962c24cc2
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 14:06:52 -0600

    tema 20

---
+++ _020__El_condicional_IF_y_operadores_de_comparación.py
@@ -0,0 +1,16 @@
+# El condicional IF y operadores de comparacion
+
+num1=16 #numero
+num2=22 #numero
+if num1 != num2: # iff
+    print('se ejecuta el if.') #imprecion strin
+
+num3=18 #numero
+num4=26 #numero
+if num3 < num4: #iff
+ print('se ejecuta el if.') #imprecion string
+ 
+num5=33 #numero
+num6=88 #numero
+if num5 <= num6: #iff
+   print('se ejecuta el if.') #imprecion del string
