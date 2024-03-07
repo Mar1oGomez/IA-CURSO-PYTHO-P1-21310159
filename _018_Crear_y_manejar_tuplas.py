c858d3d993ec9032a3ba715c908dfe5b812fa765
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 13:13:04 -0600

    tema 18

---
+++ _018_Crear_y_manejar_tuplas.py
@@ -0,0 +1,11 @@
+# 18 Crear y manejar tuplas 
+lista=['cafe', 'negro','blanco', 'verde','lila'] #lista varea, string
+print(lista)    
+
+tupla=("mole","taco","pollo","pez") #la tupla no varea,  string
+print(tupla[3])
+print(tupla)
+
+tupla2= (22,13,45,33, 'Resultado de la operacion: ')
+print(tupla2[4], tupla2[1]+ tupla2[0] - tupla2[3])
+
