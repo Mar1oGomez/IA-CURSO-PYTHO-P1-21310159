46038ead2525d9df5121872023fb44ae3144d4ac
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 19:43:15 -0600

    tema 24

---
+++ _024_Múltiples_condiciones_if.py
@@ -0,0 +1,12 @@
+# Multiples condiciones if
+dead = int(input('como quieres morir XD selecciona del 1 al 4:\n')) #string y memoria
+if dead ==1:  #variable
+    print('moriras de diarrea explosiva') #imprecion de texto
+if dead ==2: #variable
+    print('moriras de SIDA risa') #imprecion de texto
+if dead ==3 :  #variable
+    print('moriras de deprecion') #imprecion de texto
+if dead ==4:  #variable
+    print('jugaste ruleta rusa y pos te toco plomo') #imprecion de texto
+
+
