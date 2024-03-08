ceff1a5ad679a4c5ba96cf90e4d7938502d63418
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 19:30:20 -0600

    tema 23

---
+++ _023_Buscar_datos_en_listas_y_tuplas_Python.py
@@ -0,0 +1,20 @@
+#Buscar datos en listas y tuplas Python
+food = input('introduce una comida:\n')
+tupla_food=('mole','pollo','pizza','dogo','flautas','garro')
+
+if food in tupla_food[0]:
+    print('hoy se come')
+elif food in tupla_food[1]:
+    print('hoy se come')
+elif food in tupla_food[2]:
+    print('hoy se come')
+elif food in tupla_food[3]:
+    print('hoy se come')
+elif food in tupla_food[4]:
+    print('hoy se come')
+elif food in tupla_food[5]:
+    print('hoy se come')
+else:
+    print('hoy no se come')
+
+
