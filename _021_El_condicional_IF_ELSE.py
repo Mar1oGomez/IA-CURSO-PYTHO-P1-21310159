f533f155d77cc285e3aacd1f6c0da12b84fd7170
Mar1oGomez <162080679+Mar1oGomez@users.noreply.github.com>
2024-03-07 18:52:22 -0600

    tema 21

---
+++ _021_El_condicional_IF_ELSE.py
@@ -0,0 +1,7 @@
+#El condicional IF ELSE
+edad = 18 #variable edad
+if edad >= 18: #if mayor igual que
+    print("puedes entrar, eres mayor de edad.") #imprime mensaje
+    
+else:
+    print("no puedes entrar. ") #imprime un false por else
